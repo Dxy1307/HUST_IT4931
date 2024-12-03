4f089a70d009d3dc996231c86c0ee6ae6abcb263
Dxy1307 <ngducduy137@gmail.com>
2024-12-04 02:39:50 +0700

    confirm format event_time

--- csv_to_kafka.py
+++ csv_to_kafka.py
@@ -10,14 +10,14 @@ def delivery_report(err,msg):
 
 producer = Producer({'bootstrap.servers': 'localhost:9092'})
 
-csv_file = 'datasets/2019-Nov-100.csv'
+csv_file = 'E:/bigdata/datasets/2019-Nov.csv'
 topic_name = 'test'
 
 chunk_size = 1000
 previous_time = None
 
 for chunk in pd.read_csv(csv_file, chunksize=chunk_size, parse_dates=['event_time']):
-    chunk['event_time'] = chunk['event_time'].dt.strftime('%Y-%m-%d %H:%M:%S UTC')
+    chunk['event_time'] = chunk['event_time'].dt.strftime('%Y-%m-%d %H:%M:%S')
     for timestamp, rows in chunk.groupby('event_time'):
         if previous_time is not None:
             delay = (pd.to_datetime(timestamp) - pd.to_datetime(previous_time)).total_seconds()
